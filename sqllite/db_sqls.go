119b7f862197e5759b1bfc106224c5657964589b
Rafael Silva <rafaelsilvarnj@gmail.com>
2024-10-10 11:04:12 -0300

    - Fixed SQL statements - Fixed funcion calls

--- sqllite/db_sqls.go
+++ sqllite/db_sqls.go
@@ -16,21 +16,20 @@ func createRequestTableSql() SQL {
 	"project_id" integer NOT NULL,
 	"verb" TEXT,
 	"url" TEXT,
-	FOREIGN KEY("project_id") REFERENCES project("id"),
+	FOREIGN KEY("project_id") REFERENCES project("id")
 	)`}
 	return sql
 }
 
 func createResponseTableSql() SQL {
-    sql := SQL{sql: `CREATE TABLE IF NOT EXISTS response (
-        "id" integer NOT NULL,
+	sql := SQL{sql: `CREATE TABLE IF NOT EXISTS response (
+        "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
         "request_id" integer NOT NULL,
         "status_code" integer NOT NULL,
         "active" integer NOT NULL,
         "body" TEXT,
-        "mime" TEXT
+        "mime" TEXT,
         FOREIGN KEY("request_id") REFERENCES request("id")
     )`}
-    return sql
+	return sql
 }
-
