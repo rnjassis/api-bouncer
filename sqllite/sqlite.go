a5e70437c01388a195618509758cc8fddb0b7a09
Rafael Silva <rafaelsilvarnj@gmail.com>
2024-10-10 11:04:12 -0300

    - Fixed SQL statements - Fixed funcion calls

--- sqllite/sqlite.go
+++ sqllite/sqlite.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
-    "strconv"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/rnjassis/api-bouncer/models"
@@ -42,60 +42,60 @@ func GetProjects(db *sql.DB) ([]models.Project, error) {
 }
 
 func GetProjectByName(db *sql.DB, projectName string) (*models.Project, error) {
-    project := &models.Project{}
-    rows, err := selectStatement(db, getProjectByNameSql().sql, projectName)
-    if err != nil {
-        return nil, nil //TODO add error
-    }
-    defer rows.Close()
+	project := &models.Project{}
+	rows, err := selectStatement(db, getProjectByNameSql().sql, projectName)
+	if err != nil {
+		return nil, nil //TODO add error
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        err = rows.Scan(&project.Id, &project.Port, &project.Name)
+	for rows.Next() {
+		err = rows.Scan(&project.Id, &project.Port, &project.Name, &project.Description)
 		if err != nil {
 			return nil, nil //TODO add error
 		}
-    }
-    return project, nil
+	}
+	return project, nil
 }
 
 func GetRequests(db *sql.DB, projectId int) ([]models.Request, error) {
 	rows, err := selectStatement(db, getRequestsSql().sql, strconv.Itoa(projectId))
-    if err != nil {
-        return nil, nil //TODO add error
-    }
-    defer rows.Close()
+	if err != nil {
+		return nil, nil //TODO add error
+	}
+	defer rows.Close()
 
-    slice := []models.Request{}
-    for rows.Next() {
-        request := models.Request{}
-        err = rows.Scan(&request.Id, &request.Verb, &request.Url)
-        if err != nil {
-            return nil, nil //TODO add error
-        }
+	slice := []models.Request{}
+	for rows.Next() {
+		request := models.Request{}
+		err = rows.Scan(&request.Id, &request.Verb, &request.Url)
+		if err != nil {
+			return nil, nil //TODO add error
+		}
 
-        slice = append(slice, request)
-    }
+		slice = append(slice, request)
+	}
 
 	return slice, nil
 }
 
-func GetResponse(db *sql.DB, requestId int) ([]models.Response, error) {
+func GetResponses(db *sql.DB, requestId int) ([]models.Response, error) {
 	rows, err := selectStatement(db, getResponseSQL().sql, strconv.Itoa(requestId))
-    if err != nil {
-        return nil, nil //TODO add error
-    }
-    defer rows.Close()
+	if err != nil {
+		return nil, nil //TODO add error
+	}
+	defer rows.Close()
 
-    slice := []models.Response{}
-    for rows.Next() {
-        response := models.Response{}
-        err = rows.Scan(&response.Id, &response.StatusCode, &response.Active, &response.Body, &response.Mime)
-        if err!= nil {
-            return nil, nil //TODO add error
-        }
-        slice = append(slice, response)
-    }
-    return slice, nil
+	slice := []models.Response{}
+	for rows.Next() {
+		response := models.Response{}
+		err = rows.Scan(&response.Id, &response.StatusCode, &response.Active, &response.Body, &response.Mime)
+		if err != nil {
+			return nil, nil //TODO add error
+		}
+		slice = append(slice, response)
+	}
+	return slice, nil
 }
 
 func createOrOpen() *sql.DB {
@@ -115,8 +115,8 @@ func createOrOpen() *sql.DB {
 
 func createTables(db *sql.DB) {
 	execStatement(db, createProjectTableSql().sql)
-	execStatement(db, createResponseTableSql().sql)
 	execStatement(db, createRequestTableSql().sql)
+	execStatement(db, createResponseTableSql().sql)
 }
 
 func execStatement(db *sql.DB, _statement string) (sql.Result, error) {
@@ -128,8 +128,8 @@ func execStatement(db *sql.DB, _statement string) (sql.Result, error) {
 	return result, error
 }
 
-func selectStatement(db *sql.DB, statement string, params ...string) (*sql.Rows, error) {
-	rows, err := db.Query(statement, params)
+func selectStatement(db *sql.DB, statement string, params ...any) (*sql.Rows, error) {
+	rows, err := db.Query(statement, params...)
 	if err != nil {
 		return nil, err
 	}
