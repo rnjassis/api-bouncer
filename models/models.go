f0bc12f1a081a0ea99343be7b598dac792e52de0
Rafael Silva <rafaelsilvarnj@gmail.com>
2024-10-16 10:29:33 -0300

    Server Implementation - Added server implementation - Updated tables to work better with gin - Created get_project with all requests and responses

--- models/models.go
+++ models/models.go
@@ -1,24 +1,50 @@
 package models
 
+import "errors"
+
 type Project struct {
 	Id          int
-	Port        int
+	Port        string
 	Name        string
 	Description string
-	Requests   []Request
+	Requests    []Request
 }
 
 type Request struct {
-	Id     int
-	Verb   string
-	Url    string
-	Responses []Response
+	Id            int
+	RequestMethod RequestMethod
+	Url           string
+	Responses     []Response
 }
 
 type Response struct {
-    Id int
-    StatusCode int
-    Active bool
-    Mime string
-    Body string
+	Id         int
+	StatusCode int
+	Active     bool
+	Mime       string
+	Body       string
+}
+
+type RequestMethod string
+
+const (
+	GET    RequestMethod = "GET"
+	POST   RequestMethod = "POST"
+	PUT    RequestMethod = "PUT"
+	DELETE RequestMethod = "DELETE"
+)
+
+func GetStatus(value string) (RequestMethod, error) {
+	switch value {
+	case string(GET):
+		return GET, nil
+	case string(POST):
+		return POST, nil
+	case string(PUT):
+		return PUT, nil
+	case string(DELETE):
+		return DELETE, nil
+	default:
+		return "", errors.New("Request Method not found")
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,19 +1,57 @@
 package server
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/rnjassis/api-bouncer/models"
 )
 
-func main() {
-	fmt.Println("Starting server")
+func RunServer(project *models.Project) {
+	fmt.Println("Starting server ", project.Name)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	for _, request := range project.Requests {
+		routeFactory(r, request)
+	}
+	r.Run(project.Port)
+}
+
+func routeFactory(ginEngine *gin.Engine, request models.Request) error {
+	var err error
+
+	switch request.RequestMethod {
+	case models.GET:
+		err = getRoute(ginEngine, request)
+	default:
+		return errors.New("No method found for " + string(request.RequestMethod))
+	}
+
+	if err != nil {
+		return errors.New("Error creating route - " + err.Error())
+	}
+	return nil
+}
+
+func getRoute(ginEngine *gin.Engine, request models.Request) error {
+	var activeResponse *models.Response
+	for _, response := range request.Responses {
+		if response.Active {
+			activeResponse = &response
+			break
+		}
+	}
+	if activeResponse == nil {
+		return errors.New("active response not found")
+	}
+
+	ginEngine.GET(request.Url, func(c *gin.Context) {
+		getResponse(c, activeResponse)
 	})
-	r.Run()
+	return nil
+}
+
+func getResponse(ginContext *gin.Context, response *models.Response) {
+	ginContext.Data(response.StatusCode, response.Mime, []byte(response.Body))
 }
